1a62936a3ff22386d364259e0ea2df10e06ea8e9
En-naddam <abdoennaddam@gmail.com>
2024-03-08 02:05:32 +0100

    update brand blochi-events

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>2023 Blochi Events. All Rights reserved.</p>
       </div>
     </footer>
   )
